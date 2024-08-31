db841843287b06b9600d052101c6e7f2007ff8b0
Thiago <161526886+Humano123@users.noreply.github.com>
2024-08-31 17:38:08 +0200

    Add files via upload

---
+++ diretrizes.js
@@ -0,0 +1,5 @@
+if (confirm("Olá! antes de continuar usando o site, saiba que o site não será responsavel caso você crie um arquivo malicioso, nosso site oferece uma ferramenta de edição do codigo VBScript, não queremos que você crie conteudo malicioso para prejudicar você ou outra pessoa")) {
+    alert("Obrigado por usar o site <3")
+} else {
+    alert("Obrigado por usar o site <3")
+}
